dc08cf184f2e681e8c4583973cec666c38f396c1
BosonBeard <bosonbeard@gmail.com>
2023-04-30 16:24:35 +0300

    ET

--- actions/exolve-sms.js
+++ actions/exolve-sms.js
@@ -5,4 +5,4 @@ const FLAG = core.getInput('flag')
 const EXOLVE_TOKEN = core.getInput('exolve-token')
 
 
-console.log('Hello, I am an action!'+FLAG+ aaa + EXOLVE_TOKEN)
+console.log('Hello, I am an action!'+FLAG+ 'ET = ' + EXOLVE_TOKEN)
