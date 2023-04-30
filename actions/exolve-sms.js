f47a77b2b615b3664b08621dc468c693b0818329
BosonBeard <bosonbeard@gmail.com>
2023-04-30 15:41:48 +0300

    flag

--- actions/exolve-sms.js
+++ actions/exolve-sms.js
@@ -1,1 +1,5 @@
-console.log('Hello, I am an action!')
+import * as core from '@actions/core';
+// {required: true} выкинет ошибку, если input не передан
+const FLAG = core.getInput('flag', {required: false})
+
+console.log('Hello, I am an action!'+FLAG)
