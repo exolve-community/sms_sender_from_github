c6d87147ac05e94a67eb74502c0c3041a39bf80d
BosonBeard <bosonbeard@gmail.com>
2023-04-30 16:22:21 +0300

    env

--- actions/exolve-sms.js
+++ actions/exolve-sms.js
@@ -2,5 +2,7 @@ const core = require('@actions/core');
 
 // {required: true} выкинет ошибку, если input не передан
 const FLAG = core.getInput('flag')
+const EXOLVE_TOKEN = core.getInput('exolve-token')
 
-console.log('Hello, I am an action!'+FLAG)
+
+console.log('Hello, I am an action!'+FLAG+ aaa + EXOLVE_TOKEN)
